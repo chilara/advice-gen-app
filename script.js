64ec7e72580d1e1ca73805de15f57a837a4c85e6
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2022-11-23 09:32:17 +0100

    advice generate app
    advice generate app

---
+++ script.js
@@ -0,0 +1,30 @@
+let text = document.getElementById("text");
+let advise = document.getElementById("advice");
+let gen = document.getElementById("gen");
+let baseUrl = "https://api.adviceslip.com/advice";
+
+
+
+
+const generateAdvance = async () => {
+
+    try {
+  
+      const response = await fetch(baseUrl);
+      let data = await response.json();
+  
+      console.log(data);
+      text.innerHTML = `#${data.slip.advice}`;
+      advise.innerHTML = `#${data.slip.id}`;
+  
+    } catch (error) {
+  
+      console.log(error);
+  
+    }
+  
+  };
+
+
+  
+
