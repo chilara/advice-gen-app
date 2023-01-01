85a3e0e9c19339508b8a0e63093ea84fd9ef6854
Chisom Timothy Oguine <89150318+chilara@users.noreply.github.com>
2023-01-01 18:46:37 +0100

    modified javascript file

--- script.js
+++ script.js
@@ -3,28 +3,15 @@ let advise = document.getElementById("advice");
 let gen = document.getElementById("gen");
 let baseUrl = "https://api.adviceslip.com/advice";
 
-
-
-
 const generateAdvance = async () => {
+  try {
+    const response = await fetch(baseUrl);
+    let data = await response.json();
 
-    try {
-  
-      const response = await fetch(baseUrl);
-      let data = await response.json();
-  
-      console.log(data);
-      text.innerHTML = `#${data.slip.advice}`;
-      advise.innerHTML = `#${data.slip.id}`;
-  
-    } catch (error) {
-  
-      console.log(error);
-  
-    }
-  
-  };
-
-
-  
-
+    console.log(data);
+    text.innerHTML = `${data.slip.advice}`;
+    advise.innerHTML = `#${data.slip.id}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
